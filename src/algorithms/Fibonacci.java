e0e9cb0096fe9323292334c0cd4911679d8d03b2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-05-29 17:00:24 -0700

    :smiley:

--- src/algorithms/Fibonacci.java
+++ src/algorithms/Fibonacci.java
@@ -3,7 +3,29 @@ package algorithms;
 public class Fibonacci {
 public static void main(String[] args) {
 	int a=0;
-	int b=0;
-	int c=0;
+	int b=1;
+	int c;
+	
+	System.out.println(a);
+	System.out.println(b);
+	
+	
+	
+	
+	for (int i = 0; i < 12; i++) {
+		c=a+b;
+		a=b;
+		b=c;
+		System.out.println(b);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 }
--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,26 +11,46 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String name=JOptionPane.showInputDialog("What is your name?");
+		
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+		
+		name.toUpperCase();
+		
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
 
+		int length=name.length();
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
+			String character;
+			
 			
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
+			for (int i = 0; i < length; i++) {
+				if (i%2==0) {
+					character=name.substring(i, i+1);
+					character=character.toUpperCase();
+				}		
+			
+			
+			
+			
+			
+			else {
+				character=name.substring(i, i+1);
+				character=character.toLowerCase();
+			}
+			
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
-		
+			goofyName=goofyName+character;
+			}
 		// 7. Use pop-up to show user their Goofy name
-
+			JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
 
